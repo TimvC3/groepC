fde510be3f51a45479b3d021f58642f6c48cc401
Rick Hermans <rbw.hermans@student.avans.nl>
2026-04-22 14:20:35 +0200

    grid page as first page

--- Metropolis-C/routes/web.php
+++ Metropolis-C/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('grid');
 });
 
 // auth routes (login, register, etc.)
