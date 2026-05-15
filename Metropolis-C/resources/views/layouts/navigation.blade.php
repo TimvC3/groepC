2c3a7ffbb3a26ccdac204c82947bc46db1995a39
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2026-05-15 10:47:23 +0200

    Dashboard weghalen en icon veranderd. oude images verwijderd.

--- Metropolis-C/resources/views/layouts/navigation.blade.php
+++ Metropolis-C/resources/views/layouts/navigation.blade.php
@@ -3,7 +3,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('grid') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
@@ -60,9 +60,6 @@
 
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
 
             <x-responsive-nav-link :href="route('grid')" :active="request()->routeIs('grid')">
                 {{ __('Metropolis Grid') }}
